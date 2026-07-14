99b2306e2efeff4dad834eb0260222c5532a7778
camilo210981984 <camilo21098@gmail.com>
2026-07-14 12:51:47 -0500

    fix: .bat is ready to use in server

--- run_scraper.bat
+++ run_scraper.bat
@@ -1,7 +1,13 @@
 @echo off
-cd /d C:\projects\WB\Proesa
-echo Obteniendo ultimos cambios de GitHub...
+cd /d "D:\webscraping\Proesa"
+
+echo [1/3] Obteniendo ultimos cambios de GitHub...
 git pull
-echo Iniciando Suite Data Universal - Tarea Programada Diaria
+
+echo [2/3] Verificando e instalando dependencias de Python...
+pip install --disable-pip-version-check -r requirements.txt
+
+echo [3/3] Iniciando Suite Data Universal - Tarea Programada Diaria
 python main.py
+
 echo Tarea finalizada.
---
+++ run_suite.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "D:\webscraping\Proesa"
+echo iniciando suite
+python suite_app.py
+
+echo Tarea finalizada.
